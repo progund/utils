b3ed021e00d4b3b72afd1e0b8ac2cc459b40ccd3
Henrik Sandklef <hesa@sandklef.com>
2017-10-30 23:44:24 +0100

    pdf copies

--- bin/juneday-stats.sh
+++ bin/juneday-stats.sh
@@ -2,10 +2,12 @@
 
 TEMP_DIR=/tmp/junedaywiki
 DEST_DIR_BASE=/var/www/html/junedaywiki-stats
+PDF_DIR_BASE=/var/www/html/juneday-pdf
 PATH=${PATH}:.
 CURR_DIR=$(pwd)
 DOWNLOAD=true
-DEST_DIR=${DEST_DIR_BASE}/$(date '+%Y%m%d')/
+DATE=$(date '+%Y%m%d')
+DEST_DIR=${DEST_DIR_BASE}/${DATE}/
 
 mkdir -p "$DEST_DIR"
 export LOG_FILE=$DEST_DIR/juneday-stats.log
@@ -335,7 +337,7 @@ main()
         BOOK_COUNT=$(( BOOK_COUNT + 1 ))
         log_to_file "    --> handle book: $book"
         check_book $book >> $STAT_FILE
-        log_to_file "    -- handle book: $book"
+        log_to_file "    -- handle book: $book ($?)"
     done
     log_to_file "  <-- looping through books"
     echo "  ],"  >> $STAT_FILE
@@ -378,8 +380,14 @@ main()
 
     echo "    conv ${STAT_FILE} to  ${JD_STAT_FILE}"
 
+    ls -al ${STAT_FILE}
+    ls -al ${JD_STAT_FILE}
     cat ${STAT_FILE} | python -mjson.tool > ${JD_STAT_FILE}
+    echo "    conv ${STAT_FILE} to  ${JD_STAT_FILE} returned $?"
 #    jsonlint  ${JD_STAT_FILE}
+    ls -al ${STAT_FILE}
+    cp ${STAT_FILE} /tmp/stat-keep.json
+    ls -al ${JD_STAT_FILE}
     return
     
     #
@@ -402,6 +410,12 @@ main()
     printf "\n --== %s ==-- \n" "Presentation pdfs"
     echo "$TOTAL_PRES_PAGE_COUNT"
 
+
+    #
+    # Copy all pdfs
+    #
+    mkdir -p ${PDF_DIR_BASE}/${DATE}
+    cp -r ${TEMP_DIR} ${PDF_DIR_BASE}/${DATE}
 }
 
 
