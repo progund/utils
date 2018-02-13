b43dfb7b62ad1a16cf9a9752c60318f5c703c940
Henrik Sandklef <henrik@sandklef.com>
2018-02-13 12:38:42 +0100

    sortmuniq in correct order

--- vimeo/bin/vimeo-missing-at-wiki.sh
+++ vimeo/bin/vimeo-missing-at-wiki.sh
@@ -22,9 +22,9 @@ find_missing()
     mkdir -p ${TODAY_MIS_TMP}
     LATEST_JSON=$(curl -s $JSON_BASE | grep -B 1 "jd-complete" | head -1 | sed 's,[<>=],\n,g' | grep -A 2 href | tail -1 | sed 's,/,,g')
     curl -s $JSON_BASE/$LATEST_JSON/jd-stats.json > ${TODAY_MIS_TMP}/jd-stats.json
-    cat ${TODAY_MIS_TMP}/jd-stats.json |  jq -r '.books[].chapters[].videos'  | grep https | sed -e 's,/, ,g' -e 's/[",]//g'  | awk '{print $3}' | uniq | sort -u  > ${TODAY_MIS_TMP}/in-wiki.txt
+    cat ${TODAY_MIS_TMP}/jd-stats.json |  jq -r '.books[].chapters[].videos'  | grep https | sed -e 's,/, ,g' -e 's/[",]//g'  | awk '{print $3}' | sort -u | uniq  > ${TODAY_MIS_TMP}/in-wiki.txt
 
-    $SCRIPTDIR/vimeo-videos.sh | uniq | sort > ${TODAY_MIS_TMP}/all.txt
+    $SCRIPTDIR/vimeo-videos.sh | sort | uniq > ${TODAY_MIS_TMP}/all.txt
     
     grep -v -f ${TODAY_MIS_TMP}/in-wiki.txt ${TODAY_MIS_TMP}/all.txt > ${TODAY_MIS_TMP}/diff.txt
 
@@ -32,6 +32,8 @@ find_missing()
     do
         echo -n "$vid: "
         curl -s -H "Authorization: Bearer $VIMEO_BEARER" "https://api.vimeo.com/videos/$vid?fields=name" | jq '.name'
+        #echo
+        exit
     done
 }
 
