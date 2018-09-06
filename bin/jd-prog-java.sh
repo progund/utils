45685ca09284065aed3eb01b6508b8553a9d4c8b
Henrik Sandklef <hesa@sandklef.com>
2018-09-06 17:29:05 +0200

    passing additional args

--- bin/jd-prog-java.sh
+++ bin/jd-prog-java.sh
@@ -40,6 +40,6 @@ dload
 #
 # The additional argument to --course must be located at utils/etc/ in reop https://github.com/progund/utils
 #
-./${BOOTSTRAP_SCRIPT} --course programming-with-java
+./${BOOTSTRAP_SCRIPT} --course programming-with-java $*
 
 
