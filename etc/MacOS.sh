75341bbb792a1767a15cca12cacc39187fbf3c7a
Henrik Sandklef <hesa@sandklef.com>
2018-10-09 12:58:27 +0200

    perhaps add alias if NOT present

--- etc/MacOS.sh
+++ etc/MacOS.sh
@@ -2,8 +2,17 @@
 
 BASHRC=~/.bashrc
 
+DEBUG=true
+debug() {
+    if [ "$DEBUG" = "true" ]
+    then
+        echo "$*"
+    fi
+}
+
 alias_text()
 {
+    echo "alias_text() $1 | $2"
     ALIAS_NAME="$1"
     ALIAS_CMD="$2"
 
@@ -15,12 +24,17 @@ alias_text()
 
 add_alias()
 {
+    echo "add_alias_text $1 | $2"
     NAME="$1"
     CMD="$2"
 
-    if [ $(grep -c "alias ${NAME}" $BASHRC) -ne 0 ]
+    echo "Checking $BASHRC"
+    if [ $(grep -c "alias ${NAME}" $BASHRC) -eq 0 ]
     then
+        echo "Adding to $BASHRC"
         alias_text ${NAME} "${CMD}" >> $BASHRC
+    else
+        echo "NOT adding to $BASHRC"
     fi
 }
 
